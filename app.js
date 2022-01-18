a4285b38ddb1b3eda55a0f15ba6e3d2ac180ce0a
Gourav sonkar <79933807+cunningbait1Gourav@users.noreply.github.com>
2022-01-18 15:23:18 +0530

    Update app.js
    got an additional window of 116 hrs

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.get('/',(req,res)=>{
     
 });
 app.get('/answer',isLoggedIn, (req,res)=>{
-    if(Date.now()>( 1640563200000 +(23*24*60*60*1000)+(6.5*60*60*1000))){
+    if(Date.now()>( 1640563200000 +(27*24*60*60*1000)+(6.5*60*60*1000))){
         const teamname = req.user.teamName
         res.render('addwartise/huntdown',{ teamname });
     }
